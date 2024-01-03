569e6d64d06872804f6eff98f1f7907a0901e87f
bashar9709 <mdbashar9709gmail.com>
2024-01-04 02:46:47 +0600

    update

--- book_mangement/book/urls.py
+++ book_mangement/book/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from book.views import home, storebook, showBook, editBook, deleteBook
+# from book.views import home, storebook, showBook, editBook, deleteBook
+from . import views
 
 urlpatterns = [
-    path('',home),
-    path('store_book/',storebook,name='storebook'),
-    path('show_book/',showBook,name='showbook'),
-    path('edit_book/<int:id>',editBook,name='edit_book'),
-    path('delete_book/<int:id>',deleteBook,name='delete_book'),
+    # path('',views.home,),
+    # path('',views.TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', views.Mytemplateview.as_view(template_name='home.html'), name='home'),
+    # path('store_book/',views.storebook,name='storebook'),
+    path('store_book/',views.BookFormView.as_view(),name='storebook'),
+    # path('show_book/',views.showBook,name='showbook'),
+    path('show_book/',views.BookListView.as_view(),name='showbook'),
+    path('book_details/<int:id>',views.BookDetailsView.as_view(),name='details_book'),
+    # path('edit_book/<int:id>',views.editBook,name='edit_book'),
+    path('edit_book/<int:pk>',views.BookUpdateView.as_view(),name='edit_book'),
+    # path('delete_book/<int:id>',views.deleteBook,name='delete_book'),
+    path('delete_book/<int:pk>',views.DeleteBookView.as_view(),name='delete_book'),
 ]
--- book_mangement/book/views.py
+++ book_mangement/book/views.py
@@ -1,45 +1,126 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render,redirect
 from book.forms import bookStoreForm
 from book.models import BookStoreModel
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
-def home(request):
-    return render(request, 'base.html')
+# function based view
+# def home(request):
+#     return render(request, 'home.html')
 
+# class based view
+class Mytemplateview(TemplateView):
+    template_name = 'home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = {'name': 'Rahim','age':23}
+        print(kwargs)
+        context.update(kwargs) #dictionary uddate korear jonno update use kora
+        return context
 
-def storebook(request):
-    if request.method == 'POST':
-        book = bookStoreForm(request.POST)
-        if book.is_valid():
-            book.save()
-            print(book.cleaned_data)
+#funtion based view 
+# def storebook(request):
+#     if request.method == 'POST':
+#         book = bookStoreForm(request.POST)
+#         if book.is_valid():
+#             book.save()
+#             print(book.cleaned_data)
            
-            return redirect('showbook')
-        else:
+#             return redirect('showbook')
+#         else:
 
-            book = bookStoreForm()
-    else:
-        book = bookStoreForm()
-    return render(request, 'store_book.html', {'form': book})
+#             book = bookStoreForm()
+#     else:
+#         book = bookStoreForm()
+#     return render(request, 'store_book.html', {'form': book})
 
-
-def showBook(request):
-    book = BookStoreModel.objects.all()
-    print(book)
+# class based view
+# class BookFormView(FormView):
+#     template_name = 'store_book.html'
+#     form_class = bookStoreForm
+#     # success_url = reverse_lazy('showbook')
+#     def form_valid(self, form):
+#         form.save()
+#         return redirect('showbook')
+#(uporer class ar bikolpo)
+class BookFormView(CreateView):
+    model = BookStoreModel
+    template_name = 'store_book.html'
+    form_class = bookStoreForm
+    success_url = reverse_lazy('showbook')
     
-    return render(request,'show_book.html',{'data':book})
+    
+    
 
-def editBook(request,id):
-    book = BookStoreModel.objects.get(pk = id)
-    form = bookStoreForm(instance = book)
-    if request.method == 'POST':
-        form = bookStoreForm(request.POST,instance = book)
-        if form.is_valid():
-            form.save()
-            return redirect('showbook')
-    return render(request,'store_book.html',{'form':form})
+# def showBook(request):
+#     book = BookStoreModel.objects.all()
+#     print(book)
+    
+#     return render(request,'show_book.html',{'data':book})
 
-def deleteBook(request,id):
-    book = BookStoreModel.objects.get(pk = id).delete()
-    return redirect('showbook')
+
+# class based view
+class BookListView(ListView):
+    model = BookStoreModel
+    template_name = 'show_book.html'
+    context_object_name = 'data'
+    
+    # list query korar jonno
+    # def get_queryset(self):
+    #     return BookStoreModel.objects.filter(id='3')
+    
+    # def get_context_data(self,**kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context = {'rakib':BookStoreModel.objects.all().order_by('author')}
+    #     return context
+    
+    # ordering = ['-id']
+    
+    # def get_template_names(self): #template ke override korbe
+    #     if self.request.user.is_superuser:
+    #         template_name = 'superuser.html'
+    #     elif self.request.user.is_staff:
+    #         template_name = 'staffuser.html' 
+    #     else:
+    #         template_name = self.template_name
+    #     return [template_name]           
+       
+# book_details korar jonno 
+class BookDetailsView(DetailView):
+    model = BookStoreModel
+    template_name = 'book_details.html'
+    context_object_name = 'david'
+    pk_url_kwarg = 'id'
+
+# def editBook(request,id):
+#     book = BookStoreModel.objects.get(pk = id)
+#     form = bookStoreForm(instance = book)
+#     if request.method == 'POST':
+#         form = bookStoreForm(request.POST,instance = book)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('showbook')
+#     return render(request,'store_book.html',{'form':form})
+
+# class based views
+class BookUpdateView(UpdateView):
+    model = BookStoreModel
+    template_name = 'store_book.html'
+    form_class = bookStoreForm
+    success_url = reverse_lazy('showbook')
+
+
+# class based view
+class DeleteBookView(DeleteView):
+    model = BookStoreModel
+    template_name = 'delete_confirmation.html'
+    success_url = reverse_lazy('showbook')
+    
+# def deleteBook(request,id):
+#     book = BookStoreModel.objects.get(pk = id).delete()
+#     return redirect('showbook')
     
